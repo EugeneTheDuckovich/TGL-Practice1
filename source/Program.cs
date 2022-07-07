61c7f237a19fbc8092a6ca9604c866d5ed4b7c39
Eugene <zhenyagold1@gmail.com>
2022-07-07 20:36:54 +0300

    Created hierarchy of figures

--- source/Program.cs
+++ source/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Practice_1_source
+﻿using Practice_1_source.Figures;
+
+namespace Practice_1_source
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Figure[] figures = new Figure[] 
+            {
+                new Circle(3),
+                new Rectangle(5, 8),
+                new Triangle(3, 4, 5)
+            };
+
+            foreach (var figure in figures) Console.WriteLine(figure);
         }
     }
 }
